11da1cc1e20271fcacccee29260c0959f3be8857
Michael <vellidevv@gmail.com>
2020-04-01 23:07:40 -0700

    Updates

---
+++ precache-manifest.ef18bca414c00602999df2ee596176fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc707c15a6d26ae9d908c6e6ffac1720",
+    "url": "/michaels-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3d6838ade6ba17318d36",
+    "url": "/michaels-monsters-rolodex/static/css/main.ce58c312.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/michaels-monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/michaels-monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d6838ade6ba17318d36",
+    "url": "/michaels-monsters-rolodex/static/js/main.1e21dee1.chunk.js"
+  },
+  {
+    "revision": "d8ada837b04d6dd22c7b",
+    "url": "/michaels-monsters-rolodex/static/js/runtime-main.eea44180.js"
+  }
+]);
